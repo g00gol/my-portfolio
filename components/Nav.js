9da99a8e4fd210e2e1a17e8ea885adb70f18d499
Jackey Yang <jyang@castlepointanime.com>
2022-07-28 21:45:44 -0500

    create navbar

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+export default function Nav() {
+  return (
+    <div className="flex mx-auto w-2/6 py-4 justify-between text-lg">
+      <a href="#">jackey yang</a>
+
+      <div className="space-x-6">
+        <a href="#">projects</a>
+        <a href="#">blog</a>
+        <a href="#">contact</a>
+      </div>
+    </div>
+  );
+}
