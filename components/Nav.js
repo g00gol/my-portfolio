6faa2b41a728519fdf0e59c99f858b0c36deef3e
Jackey Yang <38109082+g00gol@users.noreply.github.com>
2022-07-31 23:21:25 -0500

    create mobile layout for nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,15 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
 export default function Nav() {
   return (
-    <div className="flex mx-auto w-2/6 py-4 justify-between text-lg">
+    <div className="flex mx-auto w-9/12 md:w-2/6 py-8 justify-between text-base md:text-lg">
       <a href="#">jackey yang</a>
 
-      <div className="space-x-6">
+      <button className="block md:hidden">
+        <AiOutlineMenu size={24} />
+      </button>
+
+      <div className="hidden md:block space-x-6">
         <a href="#">projects</a>
         <a href="#">blog</a>
         <a href="#">contact</a>
