e62c4dec44d8d77a9b4d090a52a663a220dafcf7
Jackey Yang <38109082+g00gol@users.noreply.github.com>
2022-08-03 20:59:42 -0500

    styling changes

--- components/Nav.js
+++ components/Nav.js
@@ -8,15 +8,15 @@ export default function Nav() {
   }
 
   return (
-    <div className="font-quicksand flex mx-auto w-9/12 md:w-2/6 py-8 justify-between text-base md:text-lg">
+    <div className="font-quicksand flex mx-auto w-9/12 lg:w-2/6 py-8 justify-between text-base lg:text-lg">
       <a href="#">jackey yang</a>
 
-      <button onClick={() => handleToggle(true)} className="block md:hidden">
+      <button onClick={() => handleToggle(true)} className="block lg:hidden">
         <AiOutlineMenu size={24} />
       </button>
 
-      <div className={toggle ? "fixed" : "hidden"}>
-        <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center md:hidden">
+      <div className={toggle ? "fixed z-50" : "hidden"}>
+        <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center lg:hidden">
           <div className="flex w-9/12 justify-end py-8">
             <button onClick={() => handleToggle(false)}>
               <AiOutlineClose size={24} />
@@ -30,7 +30,7 @@ export default function Nav() {
         </div>
       </div>
 
-      <div className="hidden md:block space-x-6">
+      <div className="hidden lg:block space-x-6">
         <a href="#">projects</a>
         <a href="#">blog</a>
         <a href="#">contact</a>
