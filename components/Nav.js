07751154487891dbeda2221610b5844353eeb43c
Jackey Yang <38109082+g00gol@users.noreply.github.com>
2022-08-03 23:54:14 -0500

    made nav fixed and z index 50

--- components/Nav.js
+++ components/Nav.js
@@ -8,32 +8,34 @@ export default function Nav() {
   }
 
   return (
-    <div className="font-quicksand flex mx-auto w-9/12 lg:w-2/6 py-8 justify-between text-base lg:text-lg">
-      <a href="#">jackey yang</a>
+    <div className="fixed w-screen top-0 z-50">
+      <div className="font-quicksand flex mx-auto w-9/12 lg:w-2/6 py-8 justify-between text-base lg:text-lg">
+        <a href="#">jackey yang</a>
 
-      <button onClick={() => handleToggle(true)} className="block lg:hidden">
-        <AiOutlineMenu size={24} />
-      </button>
+        <button onClick={() => handleToggle(true)} className="block lg:hidden">
+          <AiOutlineMenu size={24} />
+        </button>
 
-      <div className={toggle ? "fixed z-50" : "hidden"}>
-        <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center lg:hidden">
-          <div className="flex w-9/12 justify-end py-8">
-            <button onClick={() => handleToggle(false)}>
-              <AiOutlineClose size={24} />
-            </button>
-          </div>
-          <div className="flex flex-col items-center space-y-8 text-lg">
-            <a href="#">projects</a>
-            <a href="#">blog</a>
-            <a href="#">contact</a>
+        <div className={toggle ? "fixed z-50" : "hidden"}>
+          <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center lg:hidden">
+            <div className="flex w-9/12 justify-end py-8">
+              <button onClick={() => handleToggle(false)}>
+                <AiOutlineClose size={24} />
+              </button>
+            </div>
+            <div className="flex flex-col items-center space-y-8 text-lg">
+              <a href="#">projects</a>
+              <a href="#">blog</a>
+              <a href="#">contact</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="hidden lg:block space-x-6">
-        <a href="#">projects</a>
-        <a href="#">blog</a>
-        <a href="#">contact</a>
+        <div className="hidden lg:block space-x-6">
+          <a href="#">projects</a>
+          <a href="#">blog</a>
+          <a href="#">contact</a>
+        </div>
       </div>
     </div>
   );
