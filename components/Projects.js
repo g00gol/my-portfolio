3d4e7d907c199ba505ecb09d4a78d2ffa8eaf410
Jackey Yang <38109082+g00gol@users.noreply.github.com>
2022-08-03 23:54:54 -0500

    created project section component

---
+++ components/Projects.js
@@ -0,0 +1,51 @@
+import ProjectContainer from "./ProjectContainer";
+
+import CPAC from "../assets/images/project_images/cpacmockup.png";
+import LindoGourmet from "../assets/images/project_images/lindomockup.png";
+import PHB from "../assets/images/project_images/phbmockup.png";
+
+export default function Projects() {
+  return (
+    <div id="projects" className="w-11/12 mx-auto">
+      {/* Project heading */}
+      <div className="my-24">
+        <h1 className="text-3xl py-2">projects</h1>
+        <div className="w-full h-px bg-black"></div>
+      </div>
+
+      {/* Project gallery */}
+      <div className="space-y-12 lg:grid grid-cols-4 gap-8 w-full">
+        <ProjectContainer
+          image={CPAC}
+          title={"Castle Point Anime Convention"}
+          time={"feb 2022 - present"}
+          tags={["frontend", "web design"]}
+        />
+
+        <ProjectContainer />
+        <ProjectContainer />
+
+        <ProjectContainer
+          image={LindoGourmet}
+          title={"LindoGourmet"}
+          time={"jun 2022 - present"}
+          tags={["fullstack", "ecommerce", "web design"]}
+        />
+
+        <ProjectContainer />
+
+        <ProjectContainer
+          image={PHB}
+          title={"Play! Hoboken - Billiards"}
+          time={"april 2022 - may 2022"}
+          tags={["frontend", "wordpress", "web design", "crm"]}
+        />
+
+        <ProjectContainer />
+        <ProjectContainer />
+        <ProjectContainer />
+        <ProjectContainer />
+      </div>
+    </div>
+  );
+}
