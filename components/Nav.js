ea4303d3bd691f79c6302611e2f003b82040c14f
Jackey Yang <38109082+g00gol@users.noreply.github.com>
2022-08-21 14:23:38 -0500

    added background blur on scroll

--- components/Nav.js
+++ components/Nav.js
@@ -1,40 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Nav() {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setScrollPos(e.target.documentElement.scrollTop);
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollPos]);
+
   const [toggle, setToggle] = useState(false);
   function handleToggle(isToggled) {
     setToggle(isToggled);
   }
 
   return (
-    <div className="fixed w-screen top-0 z-50">
-      <div className="font-quicksand flex mx-auto w-9/12 lg:w-2/6 py-8 justify-between text-base lg:text-lg">
-        <a href="#">jackey yang</a>
-
-        <button onClick={() => handleToggle(true)} className="block lg:hidden">
-          <AiOutlineMenu size={24} />
-        </button>
-
-        <div className={toggle ? "fixed z-50" : "hidden"}>
-          <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center lg:hidden">
-            <div className="flex w-9/12 justify-end py-8">
-              <button onClick={() => handleToggle(false)}>
-                <AiOutlineClose size={24} />
-              </button>
-            </div>
-            <div className="flex flex-col items-center space-y-8 text-lg">
-              <a href="#">projects</a>
-              <a href="#">blog</a>
-              <a href="#">contact</a>
+    <div className="fixed w-full top-0 z-50">
+      <div
+        id="nav"
+        className={
+          "transition-all ease-in-out mx-auto lg:w-2/6 my-6 py-2" +
+          " " +
+          (scrollPos > 0
+            ? "bg-gray-300 bg-opacity-50 box-content rounded-full px-2"
+            : "")
+        }
+      >
+        <div className="font-quicksand flex justify-between text-base lg:text-lg">
+          <a href="#">jackey yang</a>
+          <button
+            onClick={() => handleToggle(true)}
+            className="block lg:hidden"
+          >
+            <AiOutlineMenu size={24} />
+          </button>
+          <div className={toggle ? "fixed z-50" : "hidden"}>
+            <div className="bg-gray-100 flex flex-col fixed right-0 top-0 w-screen h-screen items-center lg:hidden">
+              <div className="flex w-9/12 justify-end py-8">
+                <button onClick={() => handleToggle(false)}>
+                  <AiOutlineClose size={24} />
+                </button>
+              </div>
+              <div className="flex flex-col items-center space-y-8 text-lg">
+                <a href="#">projects</a>
+                <a href="#">blog</a>
+                <a href="#">contact</a>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="hidden lg:block space-x-6">
-          <a href="#">projects</a>
-          <a href="#">blog</a>
-          <a href="#">contact</a>
+          <div className="hidden lg:block space-x-6">
+            <a href="#">projects</a>
+            <a href="#">blog</a>
+            <a href="#">contact</a>
+          </div>
         </div>
       </div>
     </div>
