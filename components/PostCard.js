2d485768a806d60e59dc0f55fad2febd02808625
Jackey Yang <jyang@castlepointanime.com>
2022-09-14 10:06:29 -0400

    update post card with styling and data

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,10 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const PostCard = ({ post }) => {
+  const [content, setContent] = useState("");
+
+  function limitContent() {
+    const numberOfWords = 10;
+    var array = post.content.match();
+    console.log("array", array);
+    setContent(array.slice(0, numberOfWords));
+  }
+
+  useEffect(() => {
+    limitContent();
+    console.log(content);
+  }, []);
+
   return (
     <div>
-      <h1>{post.title}</h1>
-      <h1>{post.excerpt}</h1>
+      {console.log(content)}
+      <h1 className="bg-gradient-to-r from-linear1 via-linear2 to-linear3 bg-clip-text text-transparent text-xl font-playfair font-bold">
+        {post.title}
+      </h1>
+      <h1 className="font-quicksand text-lg">
+        {content}{" "}
+        <a href="" className="font-quicksand text-lg underline">
+          keep reading
+        </a>
+      </h1>
     </div>
   );
 };
